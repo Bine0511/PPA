9b2f2b363aefe0b170bac2a82debbe86d81a1b51
Bine0511 <sabine.schimpf2@aon.at>
2015-01-12 14:10:57 +0100

    0.0.3
    - pdf-l4 installiert

--- laravel/app/config/app.php
+++ laravel/app/config/app.php
@@ -122,6 +122,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'BrainSocket\BrainSocketServiceProvider',
+		'Thujohn\Pdf\PdfServiceProvider',
 
 	),
 
@@ -190,6 +191,7 @@ return array(
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
 		'BrainSocket'     => 'BrainSocket\BrainSocketFacade',
+		'PDF'             => 'Thujohn\Pdf\PdfFacade',
 
 	),
 
