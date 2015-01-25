0048ee6d311fd4aa2371fb52c213464db36ba055
Kandrina <manuela.greifoner@gmx.at>
2015-01-25 21:30:34 +0100

    0.0.11
    Versuche den array-Fehler zu beheben

--- laravel/app/controllers/FileController.php
+++ laravel/app/controllers/FileController.php
@@ -15,8 +15,8 @@ class FileController extends Controller
 				$sumvote = $sumvote + $vote->value;
 			}
 			$avgUs = $sumvote/count($vote);
-			dd($this->avgs);
-			$this->avgs = array_add($this->avgs, $ustory->userstory_ID, $avgUs);
+			//dd($this->avgs);
+			//$this->avgs = array_add($this->avgs, $ustory->userstory_ID, $avgUs);
 		}
 		return $this->avgs;
 
@@ -30,9 +30,9 @@ class FileController extends Controller
 	$user = DB::table('user')->take(10)->get();
 	$userstory = DB::table('userstory')->take(10)->get();
 
-    $html = '<html><body><table style="text-align:right; border: 2px solid #000; width:90%;"><tr><td>&nbsp;</td>';
+    $html = '<html><body><table style="text-align:right; border: 2px solid #000; width:100%;"><tr style="border-bottom: 3px solid #000"><td>&nbsp;</td>';
     foreach ($userstory as $userstory) {
-    	$html = $html . '<td>'.$userstory->userstory_name.'</td>';
+    	$html = $html . '<td width="">'.$userstory->userstory_name.'</td>';
     }
     $html = $html. '</tr>';
   	foreach ($user as $user)
@@ -45,12 +45,20 @@ class FileController extends Controller
 
     	$html = $html . '</tr>';
 	}      
-	$html = $html . '<tr style="border-top:4px solid #000;"><td>Avg</td>';
-	foreach ($this->avgs as $avgs => $value) {
-		$html = $html.'<td>'.$value.'</td>';
+	$html = $html . '<tr style="border-top:4px solid #000;"><td><b>Avg</b></td>';
+	foreach (/*$this->avgs as $avgs => $value*/$userstory as $use) {
+		$html = $html.'<td><b>'./*$value*/'avg'.'</b></td>';
 	}
-	$html = $html . '</tr>';
-    $html = $html . '</table></body></html>';
+	$html = $html . '</tr></table><br>';
+
+   /* $html = $html . '<table style="text-align:right; border: 2px solid #000; width:100%;"><tr><td>avg</td>';
+    $us = DB::table('userstory')->take(10)->get();
+    foreach ($us as $us) {
+    	$html = $html.'<td><b>'./*$value*//*$us->userstory_name.'</b></td>';
+    }
+    $html = $html . '</tr></table>';*/
+
+        $html = $html . '</body></html>';
     return PDF::load($html, 'A4', 'landscape')->show();
 }
 }
