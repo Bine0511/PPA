68b89e0d3b5101e6e9270987739efe755b3c12dc
Kandrina <manuela.greifoner@gmx.at>
2015-01-26 15:56:31 +0100

    0.0.14
    PDF Berechnungen fortgesetzt
    Datenbank mit Einträgen und ein paar Änderungen exportiert

--- db/ppa.sql
+++ db/ppa.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 4.2.7.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 26. Jan 2015 um 13:43
--- Server Version: 5.6.21
--- PHP-Version: 5.6.3
+-- Generation Time: Jan 26, 2015 at 03:54 PM
+-- Server version: 5.6.20
+-- PHP Version: 5.5.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,23 +17,23 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Datenbank: `ppa-testdb`
+-- Database: `ppa`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `moderator`
+-- Table structure for table `moderator`
 --
 
 CREATE TABLE IF NOT EXISTS `moderator` (
 `moderator_ID` int(15) NOT NULL,
   `moderator_name` varchar(50) NOT NULL,
   `moderator_pw` varchar(150) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
--- Daten für Tabelle `moderator`
+-- Dumping data for table `moderator`
 --
 
 INSERT INTO `moderator` (`moderator_ID`, `moderator_name`, `moderator_pw`) VALUES
@@ -43,7 +43,7 @@ INSERT INTO `moderator` (`moderator_ID`, `moderator_name`, `moderator_pw`) VALUE
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `session`
+-- Table structure for table `session`
 --
 
 CREATE TABLE IF NOT EXISTS `session` (
@@ -51,54 +51,64 @@ CREATE TABLE IF NOT EXISTS `session` (
   `session_name` varchar(50) NOT NULL,
   `session_pw` varchar(50) NOT NULL,
   `session_moderator_ID` int(15) NOT NULL,
-  `session_basestory_id` int(15) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `session_basestory_id` int(15) DEFAULT NULL,
+  `avg_sum` varchar(20) NOT NULL,
+  `avg_sum_base` varchar(20) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
--- Daten für Tabelle `session`
+-- Dumping data for table `session`
 --
 
-INSERT INTO `session` (`session_ID`, `session_name`, `session_pw`, `session_moderator_ID`, `session_basestory_id`) VALUES
-(1, '1234', '1234', 1, 0),
-(2, 'test', 'test', 2, 0);
+INSERT INTO `session` (`session_ID`, `session_name`, `session_pw`, `session_moderator_ID`, `session_basestory_id`, `avg_sum`, `avg_sum_base`) VALUES
+(1, '1234', '1234', 1, 1, '17.25', '15.5'),
+(2, 'test', 'test', 2, 0, '', '');
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `timevote`
+-- Table structure for table `timevote`
 --
 
 CREATE TABLE IF NOT EXISTS `timevote` (
-`timevote_vote_id` int(15) NOT NULL,
+  `timevote_user_id` int(15) NOT NULL,
   `timevote_userstory_id` int(15) NOT NULL,
   `timevote_session_id` int(15) NOT NULL,
   `timevote_value` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `timevote`
+--
+
+INSERT INTO `timevote` (`timevote_user_id`, `timevote_userstory_id`, `timevote_session_id`, `timevote_value`) VALUES
+(1, 1, 1, '20'),
+(2, 1, 1, '10');
+
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
 `user_ID` int(15) NOT NULL,
   `user_name` varchar(50) NOT NULL,
   `user_session_ID` int(15) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
--- Daten für Tabelle `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`user_ID`, `user_name`, `user_session_ID`) VALUES
 (1, 'Franz', 1),
-(2, 'Herbert', 2);
+(2, 'Herbert', 1);
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `userstory`
+-- Table structure for table `userstory`
 --
 
 CREATE TABLE IF NOT EXISTS `userstory` (
@@ -106,22 +116,22 @@ CREATE TABLE IF NOT EXISTS `userstory` (
   `userstory_session_ID` int(15) NOT NULL,
   `userstory_name` varchar(50) NOT NULL,
   `userstory_description` varchar(300) DEFAULT NULL,
-  `userstory_average` int(10) DEFAULT NULL,
+  `userstory_average` varchar(20) DEFAULT NULL,
   `userstory_time_average` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
--- Daten für Tabelle `userstory`
+-- Dumping data for table `userstory`
 --
 
 INSERT INTO `userstory` (`userstory_ID`, `userstory_session_ID`, `userstory_name`, `userstory_description`, `userstory_average`, `userstory_time_average`) VALUES
-(1, 1, 'Planung', 'Planungsdokumente erstellen', 0, ''),
-(2, 2, 'Programmierung', 'Programmieren bis zum umfallen', 0, '');
+(1, 1, 'Planung', 'Planungsdokumente erstellen', '15.5', ''),
+(2, 1, 'Programmierung', 'Programmieren bis zum umfallen', '1.75', '');
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `vote`
+-- Table structure for table `vote`
 --
 
 CREATE TABLE IF NOT EXISTS `vote` (
@@ -132,106 +142,103 @@ CREATE TABLE IF NOT EXISTS `vote` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Daten für Tabelle `vote`
+-- Dumping data for table `vote`
 --
 
 INSERT INTO `vote` (`vote_user_ID`, `vote_userstory_ID`, `vote_session_id`, `value`) VALUES
 (1, 1, 1, '12'),
-(2, 2, 2, '19');
+(1, 2, 1, '3'),
+(2, 1, 1, '19'),
+(2, 2, 1, '0.5');
 
 --
--- Indizes der exportierten Tabellen
+-- Indexes for dumped tables
 --
 
 --
--- Indizes für die Tabelle `moderator`
+-- Indexes for table `moderator`
 --
 ALTER TABLE `moderator`
  ADD PRIMARY KEY (`moderator_ID`), ADD UNIQUE KEY `moderator_ID_2` (`moderator_ID`), ADD KEY `moderator_ID` (`moderator_ID`);
 
 --
--- Indizes für die Tabelle `session`
+-- Indexes for table `session`
 --
 ALTER TABLE `session`
  ADD PRIMARY KEY (`session_ID`,`session_name`), ADD UNIQUE KEY `session_moderator_ID` (`session_moderator_ID`);
 
 --
--- Indizes für die Tabelle `timevote`
+-- Indexes for table `timevote`
 --
 ALTER TABLE `timevote`
- ADD PRIMARY KEY (`timevote_vote_id`);
+ ADD PRIMARY KEY (`timevote_user_id`);
 
 --
--- Indizes für die Tabelle `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
  ADD PRIMARY KEY (`user_ID`), ADD KEY `user_session_ID` (`user_session_ID`);
 
 --
--- Indizes für die Tabelle `userstory`
+-- Indexes for table `userstory`
 --
 ALTER TABLE `userstory`
  ADD PRIMARY KEY (`userstory_ID`,`userstory_session_ID`), ADD KEY `userstory_session_ID` (`userstory_session_ID`);
 
 --
--- Indizes für die Tabelle `vote`
+-- Indexes for table `vote`
 --
 ALTER TABLE `vote`
  ADD PRIMARY KEY (`vote_user_ID`,`vote_userstory_ID`,`vote_session_id`), ADD KEY `vote_userstory_ID` (`vote_userstory_ID`), ADD KEY `vote_session_id` (`vote_session_id`);
 
 --
--- AUTO_INCREMENT für exportierte Tabellen
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT für Tabelle `moderator`
+-- AUTO_INCREMENT for table `moderator`
 --
 ALTER TABLE `moderator`
 MODIFY `moderator_ID` int(15) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
--- AUTO_INCREMENT für Tabelle `session`
+-- AUTO_INCREMENT for table `session`
 --
 ALTER TABLE `session`
 MODIFY `session_ID` int(15) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
--- AUTO_INCREMENT für Tabelle `timevote`
---
-ALTER TABLE `timevote`
-MODIFY `timevote_vote_id` int(15) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT für Tabelle `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
 MODIFY `user_ID` int(15) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
--- AUTO_INCREMENT für Tabelle `userstory`
+-- AUTO_INCREMENT for table `userstory`
 --
 ALTER TABLE `userstory`
 MODIFY `userstory_ID` int(15) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
--- Constraints der exportierten Tabellen
+-- Constraints for dumped tables
 --
 
 --
--- Constraints der Tabelle `moderator`
+-- Constraints for table `moderator`
 --
 ALTER TABLE `moderator`
 ADD CONSTRAINT `moderator_ibfk_1` FOREIGN KEY (`moderator_ID`) REFERENCES `session` (`session_moderator_ID`);
 
 --
--- Constraints der Tabelle `user`
+-- Constraints for table `user`
 --
 ALTER TABLE `user`
 ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`user_session_ID`) REFERENCES `session` (`session_ID`);
 
 --
--- Constraints der Tabelle `userstory`
+-- Constraints for table `userstory`
 --
 ALTER TABLE `userstory`
 ADD CONSTRAINT `userstory_ibfk_1` FOREIGN KEY (`userstory_session_ID`) REFERENCES `session` (`session_ID`);
 
 --
--- Constraints der Tabelle `vote`
+-- Constraints for table `vote`
 --
 ALTER TABLE `vote`
 ADD CONSTRAINT `vote_ibfk_1` FOREIGN KEY (`vote_userstory_ID`) REFERENCES `userstory` (`userstory_ID`),
--- laravel/app/controllers/FileController.php
+++ laravel/app/controllers/FileController.php
@@ -2,61 +2,102 @@
 
 class FileController extends Controller
 {
-	public $avgs= array();
+
+
 
 	public function calculateAvg(){
-		$sumvote = 0;
-		$avgUs = 0;
-		
-		$ustory = DB::table('userstory')->take(10)->get();
+		$ustory = DB::table('userstory')->where('userstory_session_ID','=',1)->get();
 		foreach ($ustory as $ustory) {
+			$sumvote = 0;
+			$avgUs = 0;
+			$count = 0;
 			$vote = DB::table('vote')->where('vote_userstory_ID', '=', $ustory->userstory_ID)->get();
 			foreach ($vote as $vote) {
+				//dd($vote->value);
 				$sumvote = $sumvote + $vote->value;
-			}
-			$avgUs = $sumvote/count($vote);
-			//dd($this->avgs);
-			//$this->avgs = array_add($this->avgs, $ustory->userstory_ID, $avgUs);
-		}
-		return $this->avgs;
+				$count += 1;
 
+			}
+
+			
+			$avgUs = $sumvote/$count;
+			DB::table('userstory')->where('userstory_ID', '=', $ustory->userstory_ID)->update(array('userstory_average' => $avgUs));
+		}
+	}
+
+	public function sumAvg(){
+		$ustory = DB::table('userstory')->where('userstory_session_ID','=',1)->get();
+		$sum = 0;
+		foreach ($ustory as $ustory) {
+			$sum += $ustory->userstory_average;
+		}
+		DB::table('session')->where('session_ID','=',1)->update(array('avg_sum'=>$sum));
+	}
+
+	public function sumAvgBase(){
+		$baseAvg = 0;
+		$baseSum = 0;
+		$baseCount = 0;
+		$sessio = DB::table('session')->where('session_ID','=',1)->get();
+		foreach ($sessio as $sessio) {
+			$baseVote = DB::table('vote')->where('vote_userstory_ID','=',$sessio->session_basestory_id)->get();
+			foreach ($baseVote as $baseVote) {
+				$baseSum = $baseSum + $baseVote->value;
+				$baseCount += 1;
+			}
+		$baseAvg = $baseSum/$baseCount;
+		DB::table('session')->where('session_ID','=',$sessio->session_ID)->update(array('avg_sum_base'=>$baseAvg));
+		}
+		
 	}
 
 	
 	public function showPDF()
 {
 	$this->calculateAvg();
+	$this->sumAvg();
+	$this->sumAvgBase();
 
-	$user = DB::table('user')->take(10)->get();
-	$userstory = DB::table('userstory')->take(10)->get();
+	$user = DB::table('user')->where('user_session_ID','=',1)->get();
+	$userstory = DB::table('userstory')->where('userstory_session_ID','=',1)->get();
 
     $html = '<html><body><table style="text-align:right; border: 2px solid #000; width:100%;"><tr style="border-bottom: 3px solid #000"><td>&nbsp;</td>';
     foreach ($userstory as $userstory) {
     	$html = $html . '<td width="">'.$userstory->userstory_name.'</td>';
     }
     $html = $html. '</tr>';
+    
   	foreach ($user as $user)
-	{
-		$vote = DB::table('vote')->where('vote_user_id', '=', $user->user_ID)->get();
-    	$html = $html . '<tr><td>' . $user->user_name . '</td>';
+	{$html = $html . '<tr><td>' . $user->user_name . '</td>';
+		$use = DB::table('userstory')->where('userstory_session_ID','=',1)->take(10)->get();
+		foreach($use as $use){
+			$vote = DB::table('vote')->where('vote_user_ID','=',$user->user_ID)->where('vote_userstory_ID', '=', $use->userstory_ID)->get();
     	foreach ($vote as $vote) {
     		$html = $html.'<td>'.$vote->value.'</td>';
     	}
-
+		}
     	$html = $html . '</tr>';
 	}      
-	$html = $html . '<tr style="border-top:4px solid #000;"><td><b>Avg</b></td>';
-	foreach (/*$this->avgs as $avgs => $value*/$userstory as $use) {
-		$html = $html.'<td><b>'./*$value*/'avg'.'</b></td>';
+	$html = $html . '<tr style="border-top:4px solid #000;"><td><b>Durchschnitt</b></td>';
+	$usersavg = DB::table('userstory')->where('userstory_session_ID','=',1)->take(10)->get();
+	foreach ($usersavg as $usersavg) {
+		$html = $html.'<td><b>'.$usersavg->userstory_average.'</b></td>';
 	}
 	$html = $html . '</tr></table><br>';
 
-   /* $html = $html . '<table style="text-align:right; border: 2px solid #000; width:100%;"><tr><td>avg</td>';
-    $us = DB::table('userstory')->take(10)->get();
-    foreach ($us as $us) {
-    	$html = $html.'<td><b>'./*$value*//*$us->userstory_name.'</b></td>';
+
+
+   $html = $html . '<table style="text-align:right; border: 2px solid #000; width:25%;"><tr>';
+    $sess = DB::table('session')->where('session_ID','=',1)->get();
+    foreach ($sess as $sess) {
+    	$html = $html.'<td>Summe Durchschnitte</td>';
+    	$html = $html.'<td><b>'.$sess->avg_sum.'</b></td>';
+    	$html = $html.'</tr><tr><td>Summe * Avg Base</td>';
+    	$html = $html.'<td><b>'.$sess->avg_sum_base.'</b></td>';
     }
-    $html = $html . '</tr></table>';*/
+    
+
+    $html = $html . '</tr></table>';
 
         $html = $html . '</body></html>';
     return PDF::load($html, 'A4', 'landscape')->show();
