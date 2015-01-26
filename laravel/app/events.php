605b1b56d09129f803cd36c86f596b4805b39c06
GehirnArmageddon <markus.zwettler96@gmx.at>
2015-01-26 13:44:11 +0100

    DB überarbeitet

--- laravel/app/events.php
+++ laravel/app/events.php
@@ -1,4 +1,6 @@
 <?php
+
+// Default Events
 Event::listen('generic.event',function($client_data){
     return BrainSocket::message('generic.event',array('message'=>'A message from a generic event fired in Laravel!'));
 });
@@ -9,6 +11,20 @@ Event::listen('app.success',function($client_data){
 
 Event::listen('app.error',function($client_data){
     return BrainSocket::error(array('There was a Laravel App Error!'));
+});
+
+
+//Selfmade Events
+Event::listen('vote.event',function($client_data){
+    return BrainSocket::message('vote.event',array('message'=>'A User voted!'));
+});
+
+Event::listen('vote.event',function($client_data){
+    return BrainSocket::message('join.event',array('message'=>'A User joined the room!'));
+});
+
+Event::listen('vote.event',function($client_data){
+    return BrainSocket::message('story.event',array('message'=>'The next story is coming!'));
 });
 
 ?>
