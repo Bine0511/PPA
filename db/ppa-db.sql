9ca9a1de717ab223bdefbc89a494ab0857e362cf
mzwettler <markus.zwettler96@gmx.at>
2015-03-15 20:53:26 +0100

    0.0.43
    DB-Update & WebSocket HOWTO

--- db/ppa-db.sql
+++ db/ppa-db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.7.1
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 09, 2015 at 03:23 PM
--- Server version: 5.6.20
--- PHP Version: 5.5.15
+-- Erstellungszeit: 15. Mrz 2015 um 20:52
+-- Server Version: 5.6.21
+-- PHP-Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,33 +17,35 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `ppa-db`
+-- Datenbank: `ppa-db`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `moderator`
+-- Tabellenstruktur für Tabelle `moderator`
 --
 
 CREATE TABLE IF NOT EXISTS `moderator` (
 `moderator_ID` int(15) NOT NULL,
   `moderator_name` varchar(50) NOT NULL,
-  `moderator_pw` varchar(150) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `moderator_pw` varchar(150) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=667 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `moderator`
+-- Daten für Tabelle `moderator`
 --
 
-INSERT INTO `moderator` (`moderator_ID`, `moderator_name`, `moderator_pw`) VALUES
-(1, '1234moderator', '1234moderator'),
-(2, 'testmoderator', 'testmoderator');
+INSERT INTO `moderator` (`moderator_ID`, `moderator_name`, `moderator_pw`, `remember_token`) VALUES
+(1, '1234moderator', '1234moderator', NULL),
+(2, 'testmoderator', 'testmoderator', NULL),
+(666, 'Markus', 'markus', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `session`
+-- Tabellenstruktur für Tabelle `session`
 --
 
 CREATE TABLE IF NOT EXISTS `session` (
@@ -55,21 +57,23 @@ CREATE TABLE IF NOT EXISTS `session` (
   `avg_sum` varchar(20) NOT NULL,
   `avg_sum_base` varchar(20) NOT NULL,
   `avg_time_div_avg` varchar(20) NOT NULL,
-  `sum_calc_time` varchar(10) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `sum_calc_time` varchar(10) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=667 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `session`
+-- Daten für Tabelle `session`
 --
 
-INSERT INTO `session` (`session_ID`, `session_name`, `session_pw`, `session_moderator_ID`, `session_basestory_id`, `avg_sum`, `avg_sum_base`, `avg_time_div_avg`, `sum_calc_time`) VALUES
-(1, '1234', '1234', 1, 1, '12.5', '7.25', '18.125', '489.37'),
-(2, 'test', 'test', 2, 2, '13.5', '2.625', '3.92645', '106.012');
+INSERT INTO `session` (`session_ID`, `session_name`, `session_pw`, `session_moderator_ID`, `session_basestory_id`, `avg_sum`, `avg_sum_base`, `avg_time_div_avg`, `sum_calc_time`, `remember_token`) VALUES
+(1, '1234', '1234', 1, 1, '12.5', '7.25', '18.125', '489.37', ''),
+(2, 'test', 'test', 2, 2, '13.5', '2.625', '3.92645', '106.012', ''),
+(666, 'Lobby', '666', 666, NULL, '', '', '', '', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `timevote`
+-- Tabellenstruktur für Tabelle `timevote`
 --
 
 CREATE TABLE IF NOT EXISTS `timevote` (
@@ -80,7 +84,7 @@ CREATE TABLE IF NOT EXISTS `timevote` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `timevote`
+-- Daten für Tabelle `timevote`
 --
 
 INSERT INTO `timevote` (`timevote_user_id`, `timevote_userstory_id`, `timevote_session_id`, `timevote_value`) VALUES
@@ -96,29 +100,30 @@ INSERT INTO `timevote` (`timevote_user_id`, `timevote_userstory_id`, `timevote_s
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Tabellenstruktur für Tabelle `user`
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
 `user_ID` int(15) NOT NULL,
   `user_name` varchar(50) NOT NULL,
-  `user_session_ID` int(15) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  `user_session_ID` int(15) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
+-- Daten für Tabelle `user`
 --
 
-INSERT INTO `user` (`user_ID`, `user_name`, `user_session_ID`) VALUES
-(1, 'Franz', 1),
-(2, 'Herbert', 1),
-(3, 'Hiii', 2),
-(4, 'Hoo', 2);
+INSERT INTO `user` (`user_ID`, `user_name`, `user_session_ID`, `remember_token`) VALUES
+(1, 'Franz', 1, NULL),
+(2, 'Herbert', 1, NULL),
+(3, 'Hiii', 2, NULL),
+(4, 'Hoo', 2, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `userstory`
+-- Tabellenstruktur für Tabelle `userstory`
 --
 
 CREATE TABLE IF NOT EXISTS `userstory` (
@@ -130,22 +135,26 @@ CREATE TABLE IF NOT EXISTS `userstory` (
   `userstory_time_average` varchar(20) DEFAULT NULL,
   `userstory_timeavg_div_avg` varchar(6) NOT NULL,
   `calc_time` varchar(6) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `userstory`
+-- Daten für Tabelle `userstory`
 --
 
 INSERT INTO `userstory` (`userstory_ID`, `userstory_session_ID`, `userstory_name`, `userstory_description`, `userstory_average`, `userstory_time_average`, `userstory_timeavg_div_avg`, `calc_time`) VALUES
 (1, 1, 'Planung', 'Planungsdokumente erstellen', '12', '15', '3.3529', '33.374'),
 (1, 2, 'Test1', NULL, '8.5', '28.5', '3.3529', '33.374'),
+(1, 666, 'Das ist die Nummer 1', 'Nummer 1 ist einfach die beste weil Sie so super toll ist', NULL, NULL, '', ''),
 (2, 1, 'Programmierung', 'Programmieren bis zum umfallen', '0.5', '17.5', '4.5', '19.632'),
-(2, 2, 'Test2', 'dd', '5', '22.5', '4.5', '19.632');
+(2, 2, 'Test2', 'dd', '5', '22.5', '4.5', '19.632'),
+(2, 666, 'Das ist die Nummer 2', 'Nummer 2 ist einfach die beste weil Sie so super toll ist', NULL, NULL, '', ''),
+(3, 666, 'Das ist die Nummer 3', 'Nummer 3 ist einfach die beste weil Sie so super toll ist', NULL, NULL, '', ''),
+(4, 666, 'Das ist die Nummer 4', 'Nummer 4 ist einfach die beste weil Sie so super toll ist', NULL, NULL, '', '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `vote`
+-- Tabellenstruktur für Tabelle `vote`
 --
 
 CREATE TABLE IF NOT EXISTS `vote` (
@@ -156,7 +165,7 @@ CREATE TABLE IF NOT EXISTS `vote` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `vote`
+-- Daten für Tabelle `vote`
 --
 
 INSERT INTO `vote` (`vote_user_ID`, `vote_userstory_ID`, `vote_session_id`, `value`) VALUES
@@ -170,69 +179,69 @@ INSERT INTO `vote` (`vote_user_ID`, `vote_userstory_ID`, `vote_session_id`, `val
 (4, 2, 2, '8');
 
 --
--- Indexes for dumped tables
+-- Indizes der exportierten Tabellen
 --
 
 --
--- Indexes for table `moderator`
+-- Indizes für die Tabelle `moderator`
 --
 ALTER TABLE `moderator`
  ADD PRIMARY KEY (`moderator_ID`), ADD UNIQUE KEY `moderator_ID_2` (`moderator_ID`), ADD KEY `moderator_ID` (`moderator_ID`);
 
 --
--- Indexes for table `session`
+-- Indizes für die Tabelle `session`
 --
 ALTER TABLE `session`
  ADD PRIMARY KEY (`session_ID`,`session_name`), ADD UNIQUE KEY `session_moderator_ID` (`session_moderator_ID`);
 
 --
--- Indexes for table `timevote`
+-- Indizes für die Tabelle `timevote`
 --
 ALTER TABLE `timevote`
  ADD PRIMARY KEY (`timevote_user_id`,`timevote_userstory_id`,`timevote_session_id`), ADD KEY `timevote_userstory_id` (`timevote_userstory_id`), ADD KEY `timevote_session_id` (`timevote_session_id`);
 
 --
--- Indexes for table `user`
+-- Indizes für die Tabelle `user`
 --
 ALTER TABLE `user`
  ADD PRIMARY KEY (`user_ID`), ADD KEY `user_session_ID` (`user_session_ID`);
 
 --
--- Indexes for table `userstory`
+-- Indizes für die Tabelle `userstory`
 --
 ALTER TABLE `userstory`
  ADD PRIMARY KEY (`userstory_ID`,`userstory_session_ID`), ADD KEY `userstory_session_ID` (`userstory_session_ID`);
 
 --
--- Indexes for table `vote`
+-- Indizes für die Tabelle `vote`
 --
 ALTER TABLE `vote`
  ADD PRIMARY KEY (`vote_user_ID`,`vote_userstory_ID`,`vote_session_id`), ADD KEY `vote_userstory_ID` (`vote_userstory_ID`), ADD KEY `vote_session_id` (`vote_session_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT für exportierte Tabellen
 --
 
 --
--- AUTO_INCREMENT for table `moderator`
+-- AUTO_INCREMENT für Tabelle `moderator`
 --
 ALTER TABLE `moderator`
-MODIFY `moderator_ID` int(15) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `moderator_ID` int(15) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=667;
 --
--- AUTO_INCREMENT for table `session`
+-- AUTO_INCREMENT für Tabelle `session`
 --
 ALTER TABLE `session`
-MODIFY `session_ID` int(15) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `session_ID` int(15) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=667;
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT für Tabelle `user`
 --
 ALTER TABLE `user`
 MODIFY `user_ID` int(15) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
--- AUTO_INCREMENT for table `userstory`
+-- AUTO_INCREMENT für Tabelle `userstory`
 --
 ALTER TABLE `userstory`
-MODIFY `userstory_ID` int(15) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `userstory_ID` int(15) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
